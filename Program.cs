35ff382d13a5c6aff80354f3629c396c52aa52da
DengHaiYang1234 <429078038@qq.com>
2018-09-10 21:53:37 +0800

    2018.9.10
    Json存储练习

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LitJson;
+using System.IO;
+
+namespace UseLitjson
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			#region-------------------写入json---------------------------
+			string path = "json.txt";
+
+			List<Skill> list = new List<Skill>();
+			
+			Skill s = new Skill();
+			s.id = 5;
+			s.name = "龙凤呈祥";
+			s.demage = 3000;
+			list.Add(s);
+			
+			Skill sd = new Skill();
+			sd.id = 6;
+			sd.name = "天天向上";
+			sd.demage = 5000;
+			list.Add(sd);
+
+			string jsonStr = JsonMapper.ToJson(list);
+			File.WriteAllText(path, jsonStr);
+			#endregion---------------------------------------------------
+
+			#region------------------------读取json-----------------------
+			Skill[] skillList = JsonMapper.ToObject<Skill[]>(File.ReadAllText("json.txt"));
+
+			foreach (var a in skillList)
+			{
+				Console.WriteLine(a);
+			}
+
+			#endregion--------------------------------------------------
+		}
+	}
+}
---
+++ Skill.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseLitjson
+{
+	public class Skill
+	{
+		public int id { get; set; }
+		public string name { get; set; }
+		public int demage { get; set; }
+
+
+		public override string ToString()
+		{
+			return "Id:" + id + "Name:" + name + "Demage:" + demage;
+		}
+	}
+}
